3a9cbf7b5a34fad797af6eb7afbcc691d0090094
kevin <kevinli37wy@gmail.com>
2020-03-29 19:42:01 -0700

    setting up git

---
+++ cmake-build-debug/CMakeFiles/airport_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/airport_simulator.dir/main.cpp.obj"
+  "airport_simulator.exe"
+  "airport_simulator.exe.manifest"
+  "airport_simulator.pdb"
+  "libairport_simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airport_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
